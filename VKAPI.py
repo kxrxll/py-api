3473f4c516a0cd6afb9e29785a845d074c317e9d
kxrxll <kovalyovkirill@gmail.com>
2022-07-02 12:35:53 +0300

    feedback solved

--- VKAPI.py
+++ VKAPI.py
@@ -1,27 +1,48 @@
 import requests
+from datetime import datetime
 
 
 class Vk_api:
-    def __init__(self, token: str):
+    def __init__(self, token: str, owner):
         self.token = token
+        if type(owner) == int:
+            self.owner_id = owner
+        else:
+            self.owner_name = owner
+            self.owner_id = self._get_id()
 
-    def _get_params(self):
+    def _get_params_for_id(self):
         return {
             "access_token": self.token,
-            "photo_sizes": 1,
-            "v": 5.81
+            "v": 5.81,
+            "owner_id": self.owner_id,
+            "extended": 1
         }
 
+    def _get_params_for_name(self):
+        return {
+            "access_token": self.token,
+            "v": 5.81,
+            "user_ids": self.owner_name
+        }
+
+    def _get_id(self):
+        params = self._get_params_for_name()
+        response = requests.get("https://api.vk.com/method/users.get", params=params)
+        response.raise_for_status()
+        return response.json()["response"][0]["id"]
+
     def get_images(self):
-        params = self._get_params()
+        params = self._get_params_for_id()
         response = requests.get("https://api.vk.com/method/photos.getAll", params=params)
         response.raise_for_status()
-        print(response.json())
         result_response = response.json()["response"]["items"]
         result = []
         for item in result_response:
             result.append({
                 "id": item["id"],
+                "date": datetime.fromtimestamp(item["date"]).strftime("%d_%m_%y"),
+                "likes": item["likes"]["count"],
                 "url": max(item["sizes"], key=lambda x: x["width"])["url"]
             })
         return result
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import configparser
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read("settings.ini")
-    vk_api = Vk_api(config["VK"]["token"])
+    owner = input("Enter ID please: ")
+    vk_api = Vk_api(config["VK"]["token"], owner)
     ya_disk = Ya_disk(config["YaDisk"]["token"])
     images = vk_api.get_images()
     for image in images:
-        ya_disk.upload(f'/Загрузки/{image["id"]}', image["url"])
-        print(image)
+        stars = ""
+        for i in range(image["likes"]):
+            stars += "⭐"
+        ya_disk.upload(f'/Загрузки_VK/{image["date"]} {stars}', image["url"])
 
